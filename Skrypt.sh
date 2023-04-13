c3b8de76dda8f6a0231a15a72a9921ea35e3540f
Krzysztof <dsw48732@student.dsw.edu.pl>
2023-04-14 01:07:28 +0200

    Część 1 zad.3

--- Skrypt.sh
+++ Skrypt.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 if [ "$1" = "--date" ]; then
-	echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+	today=$(date +%Y-%m-%d)
+	echo "Dzisiejsza data: $today"
 else
-	echo "Użycie $0 --date"
+	echo "Użycie: $0 --date"
+fi
+
+if [ "$1" = "--logs"]; then
+	for i in {1...100}; do
+		filename="log$i.txt"
+		echo "$filename stworzony poprzez Skrypt.sh dnia $today" >> $filename
+	done
+else
+	echo "Użycie: $0 --logs"
 fi
