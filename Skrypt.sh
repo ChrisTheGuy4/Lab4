ee8f2e63422b1345f37057ba03302dae8b579c44
Krzysztof <dsw48732@student.dsw.edu.pl>
2023-04-14 01:17:26 +0200

    Część 1 zad.3

--- Skrypt.sh
+++ Skrypt.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 if [ "$1" = "--date" ]; then
-	today=$(date +%Y-%m-%d)
-	echo "Dzisiejsza data: $today"
+	echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+elif [ "$1" = "--logs" ]; then
+	if [ -z "$2" ]; then
+		echo "Użycie: $0 --logs <liczba plików>"
+	else
+		num_logs=$2
+		for i in $(seq 1 $num_logs); do
+			filename="log$i.txt"
+			echo "$filename stworzony poprzez Skrypt.sh dnia $(date +%Y-%m-%d)" >> $filename
+		done
+		echo "Utworzono $num_logs plików logx.txt"
+	fi
 else
-	echo "Użycie: $0 --date"
-fi
-
-if [ "$1" = "--logs"]; then
-	for i in {1...100}; do
-		filename="log$i.txt"
-		echo "$filename stworzony poprzez Skrypt.sh dnia $today" >> $filename
-	done
-else
-	echo "Użycie: $0 --logs"
+	echo "Użycie: $0 --date | --logs <liczba plików>"
 fi
